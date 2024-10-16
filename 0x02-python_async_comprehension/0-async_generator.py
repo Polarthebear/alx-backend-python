a43b477e0ee004a5a50c11f7fabf2f602969933d
Polarthebear <thatotae325@gmail.com>
2024-10-16 09:26:34 +0000

    async_generator coroutine taking no arguments.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ async_generator coroutine taking no args
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Define async generator coroutine"""
     for _ in range(10):
         await asyncio.sleep(1)
